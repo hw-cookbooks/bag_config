d3ce14cdf3de331acb1bc21aa5101fb773d5c710
Chris Roberts <chrisroberts.code@gmail.com>
2012-04-10 08:13:49 -0700

    Define required attribute structure

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,17 @@
-default[:bag_config][:map] = Mash.new
+default[:bag_config] = Mash.new
+default[:bag_config][:mapping] = Mash.new
+default[:bag_config][:info] = Mash.new
+
+=begin
+
+default[:bag_config][:mapping] = {'chef-client' => 'chef_client'}
+default[:bag_config][:info] = {
+  'chef-client' => {
+    'encrypted' => true,
+    'secret' => 'path/or/string',
+    'bag' => 'chef&client',
+    'item' => 'custom_config'
+  }
+}
+
+=end
