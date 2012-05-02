1948c3ae4ef9d09953c6ee0a094a4ec3a5dc497c
Chris Roberts <chrisroberts.code@gmail.com>
2012-05-02 15:41:37 -0700

    Bump tiny

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "chrisroberts.code@gmail.com"
 license          "Apache 2.0"
 description      "Provides helpers for optional data bag configuration"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.0.1"
+version          "2.0.2"
 
